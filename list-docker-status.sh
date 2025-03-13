1f6c98a23c8d1cd45babe9cf2348c3036100114e
James Leatherman <176688880+james-leatherman@users.noreply.github.com>
2025-03-13 06:26:30 -0400

    Create list-docker-status.sh

---
+++ list-docker-status.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for container in $(docker ps --format "{{.Names}}"); do
+    STATUS=$(docker inspect --format '{{.State.Health.Status}}' $container)
+    echo "Container: $container - Status: $STATUS"
+done
